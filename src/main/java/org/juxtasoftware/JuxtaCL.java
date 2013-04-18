02860f36f7268f1159317f2df586939c7b8efdfc
Lou Foster <lou@performantsoftware.com>
2013-04-18 12:23:49 -0400

    basic unit test frame work in place

--- src/main/java/org/juxtasoftware/JuxtaCL.java
+++ src/main/java/org/juxtasoftware/JuxtaCL.java
@@ -6,7 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 /**
- * Main entry point for JuxtaCL. Initializes spring context and processes command line arguments
+ * Main entry point for JuxtaCL. Initializes spring context and processes command line arguments.
+ * Accepts plain text or XML files. Preliminary XML formats; TEI and Gale
  * 
  * @author loufoster
  *
@@ -14,14 +15,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class JuxtaCL {
     public static ClassPathXmlApplicationContext context;
     
-    public static void main(String[] args) throws Exception {
-        // be sure to use the saxon parser
-        System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
-
-        // initialize application context
-        initApplicationContext();
+    public static int main(String[] args) {
+        try {
+            // be sure to use the saxon parser
+            System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
+    
+            // initialize application context
+            initApplicationContext();
+        } catch (Exception e) {
+            LoggerFactory.getLogger(JuxtaCL.class).info("JuxtaCL Failed", e);
+            return -1;
+        }
         
-        LoggerFactory.getLogger(JuxtaCL.class).info("Juxta Web service started");
+        LoggerFactory.getLogger(JuxtaCL.class).info("JuxtaCL Started");
+        return 0;
     }
 
     private static void initApplicationContext() {
--- src/test/java/org/juxtasoftware/JuxtaCLTest.java
+++ src/test/java/org/juxtasoftware/JuxtaCLTest.java
@@ -1,19 +1,22 @@
 package org.juxtasoftware;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import static org.junit.Assert.*;
 
 /**
- * Unit test for simple App.
+ * Unit test for JuxtaCLTest
  */
-public class JuxtaCLTest 
-{
-    /**
-     * Rigourous Test :-)
-     */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:/applicationContext.xml"})
+public class JuxtaCLTest {
+
     @Test
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        int out = JuxtaCL.main(null);
+        assertTrue(out == 0);
     }
 }
