3ef5996ae678fc7baecc2bc799f8633631547c1e
Lou Foster <lou@performantsoftware.com>
2013-04-22 15:48:56 -0400

    test

--- src/test/java/org/juxtasoftware/JuxtaCLTest.java
+++ src/test/java/org/juxtasoftware/JuxtaCLTest.java
@@ -90,7 +90,6 @@ public class JuxtaCLTest {
         config.addFile(testFile.getPath() );
         config.addFile(testFile2.getPath() );
         
-        System.out.println("test");
         this.juxtaCL.setConfig(config);
         int changeIdx = this.juxtaCL.doComparison();
         assertTrue("Different files have zero change index", changeIdx != 0);
